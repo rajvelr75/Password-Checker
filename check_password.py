2214d41562ac18297e6a158becca23ed501bde06
Rajvel <rajvelr755@gmail.com>
2024-05-18 00:48:54 +0530

    Api Function Added

--- check_password.py
+++ check_password.py
@@ -2,6 +2,12 @@ import requests
 import hashlib
 import sys 
 
-url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
-res = requests.get(url)
-print(res)
+
+def req_api_data():
+    url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f"Error {res.status_code}")
+    else:
+        return res
+req_api_data()
