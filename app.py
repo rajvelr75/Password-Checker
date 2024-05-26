f57cac630c2129bde0b4a99956ef459d3750ed4a
Rajvel <rajvelr755@gmail.com>
2024-05-26 23:20:52 +0530

    Done

--- app.py
+++ app.py
@@ -3,7 +3,19 @@ import check_password as ck
 
 app = Flask(__name__)
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
+@app.route('/submit',methods=['POST'])
+def submit():
+    password = (request.form.get("password"))
+    cnt = ck.main(password)
+    if(cnt):
+        res = f"{password} is found {cnt} times!! You should change it."
+    else:
+        res = f"{password} not found! You are good to go."
+    return render_template("test.html", res=res)
 
 if __name__ == "__main__":
     app.run()
